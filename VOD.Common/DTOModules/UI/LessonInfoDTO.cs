341f1123846014ac7fd6e26014bb7a42f5fbcfa6
Jack Jenkins <jackjenk1993@yahoo.com>
2019-07-08 01:30:23 -0500

    Add project files.

---
+++ VOD.Common/DTOModules/UI/LessonInfoDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VOD.Common.DTOModules.UI
+{
+    public class LessonInfoDTO
+    {
+        public int LessonNumber { get; set; }
+        public int NumberofLessons { get; set; }
+        public int PreviousVideoId { get; set; }
+        public string NextVideoId { get; set; }
+        public string  NextVideoTitle { get; set; }
+        public string NextVideoThumbnail { get; set; }
+    }
+}
---
+++ VOD.Common/DTOModules/UI/VideoDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VOD.Common.DTOModules.UI
+{
+    public class VideoDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Duration { get; set; }
+        public string ThumbNail { get; set; }
+        public string URL { get; set; }
+    }
+}
